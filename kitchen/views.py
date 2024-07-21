5faa8f206a86103ec2da054bfa32645a26da03dd
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-07-21 17:13:25 +0300

    implement registration and "login required" in views

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
 from kitchen.models import Cook, DishType, Dish
 
 
+@login_required
 def index(request):
     num_cooks = Cook.objects.count()
     num_dish_types = DishType.objects.count()
@@ -22,21 +25,21 @@ def index(request):
     return render(request, "kitchen/index.html", context=context)
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
     context_object_name = "cook_list"
     template_name = "kitchen/cooks_list.html"
     paginate_by = 5
 
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     context_object_name = "dish_types_list"
     template_name = "kitchen/dish_types_list.html"
     paginate_by = 5
 
 
-class DishListView(generic.ListView):
+class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     context_object_name = "dish_list"
     template_name = "kitchen/dish_list.html"
